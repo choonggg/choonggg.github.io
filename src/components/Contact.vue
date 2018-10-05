c05b8dcfadb135baa9f0849834c08d1be53173c3
Chong Hui <iamchoonggg@gmail.com>
2018-10-06 02:17:48 +0800

    add message

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,7 +9,7 @@
     <form action="https://formspree.io/iamchoonggg@gmail.com" method="POST">
       <input type="text" name="name" placeholder="Name">
       <input type="email" name="_replyto" placeholder="Email">
-      <textarea id="" name="" cols="30" rows="10" placeholder="Message.."></textarea>
+      <textarea id="" name="message" cols="30" rows="10" placeholder="Message.."></textarea>
       <input type="submit" class="border border-grey rounded py-2 px-4" value="Send">
     </form>
   </section>
